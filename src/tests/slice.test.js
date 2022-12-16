0d8fadf1361d2f51a04c3fc114ebc25aae1e66b3
Santeri Vainionpää-Röning <santeri.roning@tuni.fi>
2022-12-16 12:59:39 +0200

    Added more test to slice

--- src/tests/slice.test.js
+++ src/tests/slice.test.js
@@ -4,3 +4,47 @@ test('Positive slice test', () => {
     var array = [1, 2, 3, 4]
     expect(slice(array, 2)).toStrictEqual([3, 4])
 });
+
+test('Array len=0', () => {
+    var array = []
+    expect(slice(array,2)).toStrictEqual([])
+});
+
+test('Null', () => {
+    expect(slice('',5)).toStrictEqual([])
+});
+
+test('Negative splitting', () => {
+    var array = [1, 2, 3, 4]
+    expect(slice(array, -2)).toStrictEqual([3, 4])
+});
+
+test('Positive overflow', () => {
+    var array = [1, 2, 3, 4]
+    expect(slice(array, 20)).toStrictEqual([])
+});
+
+test('Negative overflow', () => {
+    var array = [1, 2, 3, 4]
+    expect(slice(array, -20)).toStrictEqual([1, 2, 3, 4])
+});
+
+test('End overflow', () => {
+    var array = [1, 2, 3, 4]
+    expect(slice(array, 2, 6)).toStrictEqual([3,4])
+});
+
+test('End neg overflow', () => {
+    var array = [1, 2, 3, 4]
+    expect(slice(array, 2, -6)).toStrictEqual([])
+});
+
+test('Splice 0', () => {
+    var array = [1, 2, 3, 4]
+    expect(slice(array, 0)).toStrictEqual([1, 2, 3, 4])
+});
+
+test('End 0', () => {
+    var array = [1, 2, 3, 4]
+    expect(slice(array, 0, 0)).toStrictEqual([])
+});
